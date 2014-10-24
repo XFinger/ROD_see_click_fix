5fa2f8b2e07e145d5341607dfe459a895bbf2937
xfinger <xfinger@gmail.com>
2014-10-24 16:43:29 -0400

    fixed defaults.rb

--- lib/defaults.rb
+++
@@ -1,8 +0,0 @@
-#defaults.rb
-#configatron environmental variables - keeps secrets secret
-
-
-########## Permit Data
- configatron.app_token = 'mhaf95ytXrn6aX7KGWZq9EOsB'      #socrata client
- configatron.client_username = 'xfinger@gmail.com'        #socrata client
- configatron.client_pass = 'Lafong7186'                   #socrata client
